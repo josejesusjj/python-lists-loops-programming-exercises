ade1df39a8f476684963a66342dc5b3e41acc499
Jose <josejesuscedeno@gmail.com>
2022-07-12 10:58:02 +0000

    04 Mixed List

--- exercises/03-flip_list/app.py
+++ exercises/03-flip_list/app.py
@@ -1,3 +1,9 @@
 arr = [45, 67, 87, 23, 5,  32, 60]
-
+new_list = []
 #your code below:
+for i in range(0,len(arr), 1):
+	new_list.append(arr[len(arr)-1-i])
+    
+print("initial list:", arr)
+print("final list:", new_list)
+
