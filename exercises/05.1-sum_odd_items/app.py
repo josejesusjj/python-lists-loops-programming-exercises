9cbd918e17ccc7372e9d4e57f8cebcdd693a5823
Jose <josejesuscedeno@gmail.com>
2022-07-13 10:54:39 +0000

    08.1 Merge List

--- exercises/05.1-sum_odd_items/app.py
+++ exercises/05.1-sum_odd_items/app.py
@@ -1,5 +1,11 @@
 arr = [4,5,734,43,45,100,4,56,23,67,23,58,45]
 
 #Your code go here:
+def sumOdds(item):
+    total = 0
+    for i in arr:
+        if i % 2 == 1:
+            total += i
+    return total
 
-
+print(sumOdds(arr))
--- exercises/07-Do_while/app.py
+++ exercises/07-Do_while/app.py
@@ -1,2 +1,9 @@
+x = 20
+while x >= 0:
+    if x == 0:
+        print('LIFTOFF')
+    elif x % 5 == 0:
+        print(str(x) + '!')
+    else: print(x)
+    x -= 1
 
-#Your code go here:
