f2d26d7c4d088b0abcde569b4dbdcf10e98ea806
Jose <josejesuscedeno@gmail.com>
2022-07-13 12:42:23 +0000

    10.1 And one and two and three

--- .learn/resets/10-Find_avg/app.py
+++ .learn/resets/10-Find_avg/app.py
@@ -1,7 +1,4 @@
 my_list = [2323,4344,2325,324413,21234,24531,2123,42234,544,456,345,42,5445,23,5656,423]
 
 #Your code here:
-average = 0
-for i in my_list:
-    average += i
-print(average/len(my_list))
+
--- exercises/08.1-Merge_list/app.py
+++ exercises/08.1-Merge_list/app.py
@@ -4,5 +4,12 @@ chunk_two = [ 'Lucas' , 'Jake','Scott','Amy', 'Molly','Hannah','Lucas']
 
 def merge_list(list1, list2):
     #Your code go here:
+    new_chunk = []
+    for i in chunk_one:
+        new_chunk.append(i)
+    for i in chunk_two:
+        new_chunk.append(i)
+    return new_chunk
+
     
 print(merge_list(chunk_one, chunk_two))
